856f5d22eb0d8e44333b230d26f40ac12696745f
yuetongalu <148395661+yuetongalu@users.noreply.github.com>
2023-12-13 21:30:21 +0000

    check this

--- log/2 Player and the testing assignment/testing.py
+++ log/2 Player and the testing assignment/testing.py
@@ -2,68 +2,49 @@ import unittest
 from OOP import Board, HumanPlayer, BotPlayer, Game
 
 class TestTicTacToe(unittest.TestCase):
-    def test_game_init_with_empty_board(self):
-        game = Game(HumanPlayer("X"), BotPlayer("O"))
-        self.assertTrue(all(board == [' '] * 9 for board in game.board.board))
+    def setUp(self):
+        self.board = Board()
+        self.player1 = HumanPlayer("X")
+        self.player2 = BotPlayer("O")
 
-    def test_game_init_with_players(self):
-        player1 = HumanPlayer("X")
-        player2 = BotPlayer("O")
-        game = Game(player1, player2)
-        self.assertEqual(game.player1, player1)
-        self.assertEqual(game.player2, player2)
+    def test_empty_board(self):
+        for row in self.board.board:
+            for cell in row:
+                self.assertEqual(cell, ' ')
 
-    def test_players_assigned_unique_pieces(self):
-        player1 = HumanPlayer("X")
-        player2 = BotPlayer("O")
-        self.assertNotEqual(player1.symbol, player2.symbol)
+    def test_initialize_game_with_two_players(self):
+        game = Game(self.player1, self.player2)
+        self.assertEqual(game.player1.symbol, "X")
+        self.assertEqual(game.player2.symbol, "O")
 
-    def test_alternate_turns(self):
-        game = Game(HumanPlayer("X"), BotPlayer("O"))
-        game.play()
-        self.assertEqual(game.current_player, game.player2)
+    def test_initialize_game_with_one_player(self):
+        game = Game(self.player1, None)
+        self.assertEqual(game.player1.symbol, "X")
+        self.assertIsNone(game.player2)
 
-    def test_winning_end_of_game_detected(self):
-        # Create a winning scenario
-        game = Game(HumanPlayer("X"), HumanPlayer("O"))
-        game.board.make_move(0, 0, "X")
-        game.board.make_move(0, 1, "O")
-        game.board.make_move(1, 1, "X")
-        game.board.make_move(1, 0, "O")
-        game.board.make_move(2, 2, "X")
-        self.assertTrue(game.board.check_winner("X"))
+    def test_players_take_turns(self):
+        game = Game(self.player1, self.player2)
+        self.assertEqual(game.current_player, self.player1)
+        game.switch_player()
+        self.assertEqual(game.current_player, self.player2)
+        game.switch_player()
+        self.assertEqual(game.current_player, self.player1)
 
-    def test_draw_game_identified(self):
-        # Create a draw scenario
-        game = Game(HumanPlayer("X"), HumanPlayer("O"))
-        game.board.make_move(0, 0, "X")
-        game.board.make_move(0, 1, "O")
-        game.board.make_move(0, 2, "X")
-        game.board.make_move(1, 0, "O")
-        game.board.make_move(1, 1, "X")
-        game.board.make_move(1, 2, "O")
-        game.board.make_move(2, 0, "O")
-        game.board.make_move(2, 1, "X")
-        game.board.make_move(2, 2, "O")
-        self.assertTrue(game.board.is_full())
-        self.assertFalse(game.board.check_winner("X"))
-        self.assertFalse(game.board.check_winner("O"))
+    def test_valid_moves(self):
+        self.assertTrue(self.board.is_valid_move(0, 0))
+        self.assertFalse(self.board.is_valid_move(3, 0))
+        self.board.make_move(0, 0, 'X')
+        self.assertFalse(self.board.is_valid_move(0, 0))
 
-    def test_players_can_play_only_in_viable_spots(self):
-        game = Game(HumanPlayer("X"), HumanPlayer("O"))
-        game.board.make_move(0, 0, "X")
-        self.assertFalse(game.board.make_move(0, 0, "O"))
+    def test_check_winner(self):
+        self.board.make_move(0, 0, 'X')
+        self.board.make_move(0, 1, 'X')
+        self.board.make_move(0, 2, 'X')
+        self.assertTrue(self.board.check_winner('X'))
 
-    def test_correct_game_winner_detected(self):
-        # Create a winning scenario
-        game = Game(HumanPlayer("X"), HumanPlayer("O"))
-        game.board.make_move(0, 0, "X")
-        game.board.make_move(0, 1, "O")
-        game.board.make_move(1, 1, "X")
-        game.board.make_move(1, 0, "O")
-        game.board.make_move(2, 2, "X")
-        self.assertEqual(game.current_player.symbol, "X")
-        self.assertTrue(game.board.check_winner("X"))
+    def test_full_board(self):
+        self.board.board = [['X', 'O', 'X'], ['X', 'O', 'X'], ['O', 'X', 'O']]
+        self.assertTrue(self.board.is_full())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
